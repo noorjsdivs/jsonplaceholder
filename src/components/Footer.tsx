1c9ef5253bd3403d871260571bdb57a578315158
Noor_Mohammad <noor.jsdivs@gmail.com>
2024-03-10 20:23:52 +0600

    footer modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,7 @@ const Footer = () => {
   return (
     <div className="bg-gray-100">
       <Container className="flex items-center justify-between">
-        <p className="text-sm">
-          Coded and maintained with ❤️ by typicode © 2024
-        </p>
+        <p className="text-sm">Coded and maintained with ❤️ by you © 2024</p>
         <p className="text-sm font-medium tracking-wide">
           All rights reserved @reactbd.com
         </p>
