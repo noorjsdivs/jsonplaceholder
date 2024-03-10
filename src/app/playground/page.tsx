9819ec0b8d15ceb04b17fb36eda83c9b4d0cd5b5
Noor_Mohammad <noor.jsdivs@gmail.com>
2024-03-10 20:45:14 +0600

    playground added

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,7 +1,16 @@
+import Container from "@/components/Container";
+import Title from "@/components/Title";
 import React from "react";
 
 const Playground = () => {
-  return <div>Playground</div>;
+  return (
+    <Container>
+      <Title title="JSONPlaceholder playground" />
+      <p className="text-sm my-2">
+        This section will be adopted to test the api integration.
+      </p>
+    </Container>
+  );
 };
 
 export default Playground;
