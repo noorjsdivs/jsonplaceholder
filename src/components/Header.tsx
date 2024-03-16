07282359f380b35e5380eaad20785f13350cd4f8
Tazim-01 <tx1trx@gmail.com>
2024-03-16 23:08:32 +0600

    new logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,6 +30,7 @@ const Header = () => {
   return (
     <div className="border-b-[1px] border-b-gray-300 bg-white shadow-sm sticky top-0 z-50">
       <Container className="flex flex-col md:flex-row items-start md:items-center justify-between">
+      <p className="text-3xl -ml-32 -mr-32 font-bold">T<span className="text-red-600">X</span></p>
         <Logo />
         <div className="flex items-center gap-x-5">
           {navigation.map((item) => (
