9b23f7a2c6af5ffbb23826ff15f2f68f859d1090
Rakib Sheikh <sheikhrakib883@gmail.com>
2024-03-20 19:57:16 +0600

    data

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,7 +6,7 @@ const Banner = () => {
     <Container className="py-20 flex flex-col gap-y-5">
       <h1 className="text-6xl mb-10">{`{JSON} Placeholder`}</h1>
       <p className="text-xl">
-        Free fake and reliable API for testing and prototyping.
+        Free fake and reliable API for testing and prototyping.pp
       </p>
       <p className="text-xl">
         Powered by <span className="underline font-medium">JSON Server</span> +{" "}
